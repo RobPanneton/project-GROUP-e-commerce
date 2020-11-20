ed2f8a0b987b31807f549b9a18ab37b3466fb6d5
Gordon Krieger <gordon.krieger@gmail.com>
2020-11-20 12:44:08 -0500

    draft Error page

--- client/src/components/errorpage/Errorpage.js
+++ client/src/components/errorpage/Errorpage.js
@@ -1,5 +1,51 @@
 import React from "react";
+import styled from "styled-components"
+import {COLORS, MARGINS} from "../../constants"
 
 export const Errorpage = () => {
-  return <div>Errorpage</div>;
+  return (
+    <Wrapper>
+      <FourOhFour>404</FourOhFour>
+  <NotFound>Page not found</NotFound>
+    <Oops>OOPS!</Oops>
+<ErrorMessage>The page you are looking for doesn't exist, or some other error occurred.</ErrorMessage>
+    </Wrapper>
+  )
 };
+
+// 
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;  
+  align-items: center;
+  margin: 0 11px 0 11px;
+`;
+
+// box-shadow could be better
+const Oops = styled.div`
+  background-color: ${COLORS.babyBlue};
+  font-size: xx-large;
+  color: ${COLORS.white};
+  font-weight: 900;
+  padding: 20px;
+  border-radius: 25% /50%;
+  margin: 20px;
+  box-shadow: rgba(0,0,0,0.8) 0 0 10px;   
+`;
+
+const FourOhFour = styled.p`
+  font-size: xx-large;
+  font-weight: 600;
+`;
+
+const NotFound = styled.p`
+  font-size: x-large;
+  font-weight: 400;
+`;
+
+const ErrorMessage = styled.p`
+font-style: italic;
+`;
