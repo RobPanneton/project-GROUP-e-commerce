f5baf9d802aea7e75f99f2e9332487aa5e412268
Zachary Nugent <63797719+MixReferences@users.noreply.github.com>
2020-11-20 13:58:59 -0500

    add header display

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,5 +1,264 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+import logo from "../../assets/logo.svg";
+import searchIcon from "../../assets/search-icon.svg";
+import cartIcon from "../../assets/cart-icon.svg";
+import styled from "styled-components";
+import { COLORS, MARGINS, BORDER_RADIUS } from "../../constants";
 
 export const Header = () => {
-  return <div>Header</div>;
+  //To be put in the store
+  const [isHamOpen, setIsHamOpen] = React.useState(false);
+  return (
+    <HeaderContainer>
+      <LogoContainer exact to="/">
+        <img src={logo} alt="Health Watch Logo" />
+        HEALTHwatch
+      </LogoContainer>
+
+      <SearchAndHamMenu>
+        <StylessButton onClick={() => setIsHamOpen(!isHamOpen)}>
+          <HamBarMenu>
+            <HamBar />
+            <HamBar />
+            <HamBar />
+          </HamBarMenu>
+        </StylessButton>
+        <SearchBarContainer>
+          <label htmlFor="search">
+            <img src={searchIcon} alt="Click to enter search query" />
+          </label>
+          <SearchInput
+            type="text"
+            name="search"
+            id="search"
+            placeholder="What are you looking for?"
+          />
+        </SearchBarContainer>
+      </SearchAndHamMenu>
+      <LoginAndCart>
+        <StylessButton>Login</StylessButton>
+        <StylessButton>
+          <img src={cartIcon} alt="Click to view cart" />
+        </StylessButton>
+      </LoginAndCart>
+      {isHamOpen && (
+        <DropDownBackground
+          tabIndex="0"
+          aria-label="Click to close navigation menu"
+          onClick={(e) => {
+            e.stopPropagation();
+            setIsHamOpen(!isHamOpen);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.stopPropagation();
+              setIsHamOpen(!isHamOpen);
+            }
+          }}
+        >
+          <DropDownMenu
+            tabIndex="0"
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.stopPropagation();
+              }
+            }}
+          >
+            <ListConatiner>
+              <NavLinkContainer to="/shop">
+                <NavItems
+                  style={{
+                    borderBottom: `1px solid ${COLORS.grey}`,
+                    borderTop: `1px solid ${COLORS.grey}`,
+                  }}
+                >
+                  Shop
+                </NavItems>
+              </NavLinkContainer>
+              <NavLinkContainer to="/companies">
+                <NavItems
+                  style={{
+                    borderBottom: `1px solid ${COLORS.grey}`,
+                  }}
+                >
+                  Partner companies
+                </NavItems>
+              </NavLinkContainer>
+              <NavLinkContainer to="/contact">
+                <NavItems
+                  style={{
+                    borderBottom: `1px solid ${COLORS.grey}`,
+                  }}
+                >
+                  Contact us
+                </NavItems>
+              </NavLinkContainer>
+              <NavLinkContainer to="/about">
+                <NavItems>About</NavItems>
+              </NavLinkContainer>
+            </ListConatiner>
+          </DropDownMenu>
+        </DropDownBackground>
+      )}
+    </HeaderContainer>
+  );
 };
+
+const HeaderContainer = styled.div`
+  background: ${COLORS.shortGreyGradient};
+  height: 54px;
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 ${MARGINS.mobileSides};
+`;
+
+const SearchAndHamMenu = styled.div`
+  position: absolute;
+  height: 54px;
+  top: 54px;
+  left: 0;
+  z-index: -1;
+  background: #fff;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 ${MARGINS.mobileSides};
+`;
+
+const LogoContainer = styled(Link)`
+  display: flex;
+  align-items: center;
+  font-family: "Montserrat Alternates";
+  font-weight: 700;
+  font-size: 18px;
+  color: ${COLORS.white};
+  text-decoration: none;
+`;
+
+const StylessButton = styled.button`
+  border: none;
+  background: transparent;
+  color: ${COLORS.white};
+  border: 1px solid transparent;
+
+  &:focus {
+    outline: none;
+    border: 1px solid ${COLORS.lightBabyBlue};
+  }
+
+  &:active {
+    transform: scale(0.98);
+  }
+`;
+
+const LoginAndCart = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 90px;
+`;
+
+const HamBarMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 18px;
+`;
+
+const HamBar = styled.div`
+  height: 2px;
+  width: 30px;
+  background: ${COLORS.navyBlue};
+`;
+
+const SearchBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  min-width: 244px;
+  padding: 2px 7px;
+  border-radius: 20px;
+  border: 1px solid ${COLORS.navyBlue};
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  font-size: 14px;
+  border: none;
+  line-height: 25px;
+  border-radius: 20px;
+  padding-left: 7px;
+  border: 1px solid transparent;
+
+  &:focus {
+    outline: none;
+    border: 1px solid ${COLORS.lightBabyBlue};
+  }
+`;
+
+const DropDownMenu = styled.div`
+  position: fixed;
+  left: 0;
+  top: 54px;
+  min-height: 216px;
+  width: 100%;
+  background: ${COLORS.longGreyGradient};
+  border-radius: 0 0 ${BORDER_RADIUS.mediumCorner} ${BORDER_RADIUS.mediumCorner};
+  animation: dropDown 0.5s;
+
+  &:focus {
+    outline: none;
+  }
+
+  @keyframes dropDown {
+    from {
+        top: -1000px;
+    }
+    to {
+        top: 54px;
+    }
+`;
+
+const DropDownBackground = styled.div`
+  position: fixed;
+  left: 0;
+  top: 54px;
+  height: 100vh;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.2);
+  animation: fadeIn 0.5s;
+
+  @keyframes fadeIn {
+    from {
+      background: rgba(0, 0, 0, 0);
+    }
+    to {
+      background: rgba(0, 0, 0, 0.2);
+    }
+  }
+`;
+
+const ListConatiner = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+const NavLinkContainer = styled(NavLink)`
+  text-align: center;
+  text-decoration: none;
+  color: ${COLORS.white};
+  font-weight: 700;
+`;
+const NavItems = styled.li`
+  padding: 25px 0;
+
+  &:hover {
+    opacity: 0.5;
+  }
+`;
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,21 +1,23 @@
 export const COLORS = {
-    black: "#222224",
-    grey: "#44454A",
-    darkestBlue: "#12232E",
-    navyBlue: "#203647",
-    babyBlue: "#007CC7",
-    lightBabyBlue: "#4DA8DA",
-    whiteBlue: "#EEFBFB",
-    white: "#ffffff",
-    shortGreyGradient: "linear-gradient(91.66deg, #222224 1.97%, #44454A 100%);",
-    longGreyGradient: " linear-gradient(100.34deg, #222224 -14.15%, #434449 90.21%);"
-}
+  black: "#222224",
+  grey: "#44454A",
+  darkestBlue: "#12232E",
+  navyBlue: "#203647",
+  babyBlue: "#007CC7",
+  lightBabyBlue: "#4DA8DA",
+  whiteBlue: "#EEFBFB",
+  white: "#ffffff",
+  shortGreyGradient: "linear-gradient(91.66deg, #222224 1.97%, #44454A 100%);",
+  longGreyGradient:
+    " linear-gradient(100.34deg, #222224 -14.15%, #434449 90.21%);",
+};
 
 export const MARGINS = {
-    mobileSides = "11px",
-}
+  mobileSides: "11px",
+  mobileTop: "108px",
+};
 
 export const BORDER_RADIUS = {
-    smallCorner = "4px",
-    mediumCorner = "7px",
-}
+  smallCorner: "4px",
+  mediumCorner: "7px",
+};
