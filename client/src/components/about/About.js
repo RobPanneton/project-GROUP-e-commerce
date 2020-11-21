c6df2507b59624bf030f8f86f5ce3c4aa9047149
Gordon Krieger <gordon.krieger@gmail.com>
2020-11-21 15:05:00 -0500

    About page + added image to assets

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,5 +1,60 @@
 import React from "react";
+import styled from "styled-components";
+
+import { COLORS, MARGINS, BORDER_RADIUS } from "../../constants";
+import aboutPhoto from "../../assets/aboutPhoto.jpg";
 
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <Wrapper>
+      <Top>About us</Top>
+      <ImageContainer>
+        <CompanyImage src={aboutPhoto} />
+      </ImageContainer>
+      <Text>
+        We are all about staying healthy while looking great. Our goal is to
+        make you feel good about yourself.
+      </Text>
+
+      <Text>
+        While our little gadgets are only little gadgets, they have the
+        potential to hold much more meaning than that.
+      </Text>
+
+      <Text>
+        We hope to help as much as we can in your life journey to bettering
+        youself.
+      </Text>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  padding-top: ${MARGINS.mobileTop};
+  margin-left: ${MARGINS.mobileSides};
+  margin-right: ${MARGINS.mobileSides};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Top = styled.div`
+  font-size: x-large;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+// keeps flexbox from mutating image
+const ImageContainer = styled.div``;
+
+const CompanyImage = styled.img`
+  height: 150px;
+  border-radius: ${BORDER_RADIUS.mediumCorner};
+`;
+
+const Text = styled.p`
+  text-align: center;
+  margin-top: 10px;
+  font-size: smaller;
+`;
